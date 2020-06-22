c080950d15070c1901f5730adedb23c308a54c23
Carlos Andrew Costa Bezerra <andrew@r2asistemas.com.br>
2020-06-22 15:14:39 -0300

    Adicionando arquivo produzidos na aula.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_TPL
+{
+    class Program
+    {
+        public static string WordFirstLetterToEndplusayAndLowerPlusAy(string word)
+        {
+
+            var palavramodificada = word.Substring(1, word.Length - 1) + word.Substring(0, 1) + "ay";
+            return palavramodificada.ToLower();
+
+        }
+        private static string[] Map(string sentence) => sentence.Split();
+
+
+        private static string[] Process(string[] words)
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                int index = i;
+                Task.Factory.StartNew(() =>
+                 {
+                     words[index] = WordFirstLetterToEndplusayAndLowerPlusAy(words[index]);
+                 }, TaskCreationOptions.AttachedToParent | TaskCreationOptions.LongRunning);
+
+            }
+            return words;
+        }
+        private static string Reduce(string[] words)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var word in words)
+            {
+                sb.Append(word);
+                sb.Append(' ');
+            }
+            return sb.ToString();
+        }
+        static void Main(string[] args)
+        {
+            var sentence = "the quick brown fox jumped over the lazy dog";
+            var task = Task<string[]>.Factory.StartNew(() => Map(sentence))
+               .ContinueWith<string[]>(t => Process(t.Result))
+               .ContinueWith<string>(t => Reduce(t.Result));
+
+            Console.WriteLine($"Result {task.Result}");
+        }
+    }
+}
